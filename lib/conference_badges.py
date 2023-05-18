6f3c67c554c7b7503713bbbe5e52e1fb28f68531
Sofia Katzman <katzmansof@gmail.com>
2023-05-17 22:23:42 -0400

    finished lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,21 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    new_badges = [f"Hello, my name is {name}." for name in names]
+    return new_badges
 
 def assign_rooms(names):
-    return None
+    rooms = range(1, 9)
+    assignments = []
+    for room in rooms:
+        assignments.append(f'Hello, {names[room - 1]}! You\'ll be assigned to room {room}!')
+    
+    return assignments
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+    
+    for assignment in assign_rooms(names):
+        print(assignment)
